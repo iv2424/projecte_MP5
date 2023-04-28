b2ad74c70734c8f96b37ceaf2a44bc6b3a023687
iv2424 <114505434+iv2424@users.noreply.github.com>
2023-04-28 17:07:58 +0200

    Update heapsort.py

--- python_implementation/heapsort.py
+++ python_implementation/heapsort.py
@@ -1,5 +1,5 @@
 class HeapSort:
-    """Representra el HeapSort que ordenarà la llista."""
+    """Representa el HeapSort que ordenarà la llista."""
     def __init__(self, llista: list) -> None:
         pass
 
